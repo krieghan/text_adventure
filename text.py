35328a2f71d20dd0f46fd0095ff536f80a43d607
Krieghan Riley <krieghan.riley@gmail.com>
2011-05-24 07:14:40 -0700

    added a communicator class to abstract display

--- text.py
+++ text.py
@@ -2,23 +2,36 @@ import re
 
 allWhitespace = re.compile('\s+')
 
-def process(textToProcess):
+class StandardCommunicator(object):
+    def __init__(self,
+                 wrapLength=80):
+        self.wrapLength = wrapLength
+        
+    def output(self,
+               textToOutput):
+        textToOutput = process(textToOutput, self.wrapLength)
+        print textToOutput
+
+def process(textToProcess,
+            wrapLength):
     textToProcess = re.sub(allWhitespace, ' ', textToProcess)
-    textToProcess = applyLineBreaks(textToProcess, 80)
+    textToProcess = applyLineBreaks(textToProcess, wrapLength)
     return textToProcess
 
 def applyLineBreaks(textToProcess, charactersPerLine):
     currentIndex = 0
     lines = []
-    toIndex = -1
-    while (currentIndex + charactersPerLine) < len(textToProcess):
-        currentIndex = toIndex + 1
-        toIndex = findIndexOfLastSpace(textToProcess, currentIndex)
+    toIndex = findIndexOfLastSpace(textToProcess, charactersPerLine) + 1
+    while currentIndex <= len(textToProcess):
         line = textToProcess[currentIndex:toIndex]
         lines.append(line)
-    return textToProcess
-    
+        currentIndex = toIndex
+        toIndex = findIndexOfLastSpace(textToProcess, toIndex + charactersPerLine) + 1
+    return '\n'.join(lines)
+
 def findIndexOfLastSpace(textToProcess, currentIndex):
+    if currentIndex > len(textToProcess):
+        return len(textToProcess)
     currentCharacter = textToProcess[currentIndex]
     while currentCharacter != ' ':
         currentIndex -= 1
